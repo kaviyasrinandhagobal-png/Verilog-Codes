3c60ece9597127588dc8ce13d0e7a15d49be3661
KAVIYASRI N <kaviyasrinandhagobal@gmail.com>
2026-08-14 16:42:52 +0530

    Add files via upload

---
+++ combitional/tb_fa.v
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02.06.2026 17:36:35
+// Design Name: 
+// Module Name: tb_fa
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module fa_tb;
+reg[3:0]a;
+reg[3:0]b; 
+reg cin; 
+wire[3:0]s;
+wire cy;
+fa_4 uut(".a(a),.b(b),.cin(cin),.s(s),.cy(cy)"); 
+initial
+begin
+a = 4'b0010;
+b = 4'b0001;
+cin = 0;
+#10
+a = 4'b0001;
+b = 4'b0001;
+cin = 0;
+#10
+a = 4'b1111;
+b = 4'b0001;
+cin=1; 
+#10 
+end
+initial
+begin
+$display("time,a,b,s,cin,s,cy"); 
+$monitor("%0t,%d,%d,%d,%d,%d",$time,a,b,cin,s,cy);#50
+$finish;
+end  
+endmodule 
