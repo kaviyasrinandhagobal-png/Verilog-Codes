d17fb20222698f7fc852e23ab2994b9e6b92639e
KAVIYASRI N <kaviyasrinandhagobal@gmail.com>
2026-08-15 18:28:42 +0530

    Update mux_tb.v

--- combitional/Mux/mux_tb.v
+++ combitional/Mux/mux_tb.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.07.2026 16:59:01
-// Design Name: 
-// Module Name: mux_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module mux_tb;
 wire y;
 reg[15:0]d;
