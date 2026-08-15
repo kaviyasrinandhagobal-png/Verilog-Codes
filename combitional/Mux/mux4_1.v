724f94a8beefb2bfe414db1c1b9ddfd105d022a8
KAVIYASRI N <kaviyasrinandhagobal@gmail.com>
2026-08-15 18:27:45 +0530

    Update and rename combitional/mux4_1.v to combitional/Mux/mux4_1.v

--- combitional/Mux/mux4_1.v
+++ combitional/Mux/mux4_1.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.07.2026 14:44:01
-// Design Name: 
-// Module Name: mux4_1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module mux(y,d,s);
 output y;
 input[9:0]s;
