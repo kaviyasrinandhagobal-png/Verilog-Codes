2f0690223d39b782687b24941b8240cd05aa83b6
KAVIYASRI N <kaviyasrinandhagobal@gmail.com>
2026-08-18 17:05:19 +0530

    Update mult_tb.v

--- combitional/mux/mult_tb.v
+++ combitional/mux/mult_tb.v
@@ -1,23 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.08.2026 17:14:02
-// Design Name: 
-// Module Name: mult_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-/////////////////////////////////////////////////////////////////////////////////
 module mult_tb;
 reg [1:0]a;
 reg b;
