5068117680135699286406e24b11a445d6f46ee9
KAVIYASRI N <kaviyasrinandhagobal@gmail.com>
2026-08-31 16:55:29 +0530

    Update bcd.v

--- combitional/BCD/bcd.v
+++ combitional/BCD/bcd.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.07.2026 22:38:01
-// Design Name: 
-// Module Name: bcd
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module bcd(y,c,s);
     input  c;
     input  [3:0] s;
