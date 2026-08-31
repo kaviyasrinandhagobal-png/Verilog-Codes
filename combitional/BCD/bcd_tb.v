d24324d9269befa617ec3cdb48ecd3a82830160a
KAVIYASRI N <kaviyasrinandhagobal@gmail.com>
2026-08-31 16:55:49 +0530

    Update bcd_tb.v

--- combitional/BCD/bcd_tb.v
+++ combitional/BCD/bcd_tb.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.07.2026 22:54:45
-// Design Name: 
-// Module Name: bcd_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module bcd_tb;
 wire y;
 reg c;
