251ddc526e32ffa0c3459a4de6b55a903f71dc12
KAVIYASRI N <kaviyasrinandhagobal@gmail.com>
2026-09-01 20:20:25 +0530

    Update gen_tb.v

--- combitional/Generator/gen_tb.v
+++ combitional/Generator/gen_tb.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.07.2026 23:15:26
-// Design Name: 
-// Module Name: gen_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module gen_tb;
 reg a,b,c;
 wire p;
